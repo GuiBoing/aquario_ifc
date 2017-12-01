c19932805aa54d2500ad7facd16f81080a18a128
GuiBoing <guilhermeboing@hotmail.com>
2017-12-01 08:10:08 -0200

    v1
    exercicios feitos 
    1
    2

---
+++ trab programacao/modeloLojaIFC-master/app/models/CrudProdutos.php
@@ -0,0 +1,48 @@
+<?php
+
+
+require_once "Conexao.php";
+require_once "Produto.php";
+
+class CrudProdutos {
+
+    private $conexao;
+    private $produto;
+
+    public function __construct() {
+        $this->conexao = Conexao::getConexao();
+    }
+
+    public function salvar(Produto $produto){
+        $sql = "INSERT INTO tb_produtos (nome, preco, categoria, qtd_estoque) VALUES ('$produto->nome', $produto->preco, '$produto->categoria', '$produto->estoque')";
+
+        $this->conexao->exec($sql);
+    }
+
+    public function buscarProduto(int $codigo){
+        $consulta = $this->conexao->query("SELECT * FROM tb_produtos WHERE codigo = $codigo");
+        $produto = $consulta->fetch(PDO::FETCH_ASSOC);
+
+        return $produto;
+
+    }
+
+    public function getProduto(int $codigo){
+        $consulta = $this->conexao->query("SELECT * FROM tb_produtos WHERE id = $codigo");
+        $prod = $consulta->fetch(PDO::FETCH_ASSOC);
+
+        return new Produto($prod['nome'], $prod['preco'], $prod['categoria'], $prod['estoque'], $prod['codigo']);
+    }
+
+    public function getProdutos(){
+        $consulta = $this->conexao->query("SELECT * FROM tb_produtos");
+        $resultados = $consulta->fetchAll(PDO::FETCH_ASSOC);
+
+        $listaNova = [];
+        foreach($resultados as $prod){
+            $listaNova[] = new Produto($prod['nome'], $prod['preco'], $prod['categoria'], $prod['estoque'], $prod['codigo']);
+        }
+        return $listaNova;
+    }
+}
+
